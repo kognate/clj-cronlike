002cecb076d53f55ff453e989445e47ec45f17b8
Joshua Smith <kognate@gmail.com>
2012-03-05 23:14:51 -0500

    initial commit

---
+++ src/cl_cronlike/core.clj
@@ -0,0 +1,1 @@
+(ns cl-cronlike.core)
---
+++ test/cl_cronlike/test/core.clj
@@ -0,0 +1,6 @@
+(ns cl-cronlike.test.core
+  (:use [cl-cronlike.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
