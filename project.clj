85b8b52970d7b8205762e48c584c1f016597918e
Joshua Smith <kognate@gmail.com>
2012-03-05 23:43:56 -0500

    added clojars dep and pushed to clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject cl-cronlike "0.0.1"
   :description "Cron-style scheduler for clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[codox "0.5.0"]])
+  :dev-dependencies [[codox "0.5.0"]
+                     [lein-clojars "0.6.0"]])
