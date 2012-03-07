6ebbac868b8ee493431b14500ed377660b034580
Joshua Smith <kognate@gmail.com>
2012-03-06 23:11:59 -0500

    bumped minor version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cl-cronlike "0.0.1"
+(defproject cl-cronlike "0.0.2"
   :description "Cron-style scheduler for clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[codox "0.5.0"]
