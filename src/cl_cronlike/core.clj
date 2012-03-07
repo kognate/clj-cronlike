964b5aedc7e1283725449cc2525342a616bc3eb1
Joshua Smith <kognate@gmail.com>
2012-03-06 23:02:22 -0500

    updated docs.

--- src/cl_cronlike/core.clj
+++ src/cl_cronlike/core.clj
@@ -19,7 +19,7 @@
                  "Sat" 7
                  "Sun" 1})
 
-(defn integerize
+(defn ^{:no-doc true} integerize
   "helper function to parse integers from strings and handle special cases
 of substrings that are really number-like representations (for days of week, mostly)"
   [v]
@@ -27,7 +27,7 @@ of substrings that are really number-like representations (for days of week, mos
     (integermap v)
     (java.lang.Integer/parseInt v)))
 
-(defn split-or-splat
+(defn ^{:no-doc true} split-or-splat
   "For cron-like strings,  if the value is a splat (which is a *) then don't split
 it on commas. Otherwise, split the field on commas"
 [strfrag]
@@ -43,22 +43,24 @@ it on commas. Otherwise, split the field on commas"
 Format is:  minutes hours day-of-month month day-of-week.
 Example:  0,15,30,45 12,13,14 * * 2,4,6
           That format means run every 15 minutes between 12:00 and 15:00 on Monday, Wednesday and Friday
-" [fstring]
-  (let [tokens (clojure.string/split fstring #"[\s]+")
+NOTE: each set of comma-seperated values MUST NOT have spaces
+" [schedulestring]
+  (let [tokens (clojure.string/split schedulestring #"[\s]+")
         [minutes hours dom mon dow] (map split-or-splat tokens)]
     (Schedule. minutes hours dom mon dow)))
 
 (defn run-function-with-cron
+  "Adds a task to the queue using a cron-style string and a function name"
   [schedulestring functorun]
   (swap! *taskdb* (fn [v]
                     (conj v (ScheduledTask. (schedule-from-string schedulestring) functorun)))))
 
-(defn ^{:nodoc true} match-field
+(defn ^{:no-doc true} match-field
   [field fval sched]
   (or (= :splat (field sched))
       (not (= nil (some (fn [v] (= fval v)) (field sched))))))
 
-(defn runs-now?
+(defn ^{:no-doc true} runs-now?
   "Check to see if a given schedule runs at this moment."
   ([sched]
      (runs-now? sched (Calendar/getInstance)))
@@ -70,19 +72,19 @@ Example:  0,15,30,45 12,13,14 * * 2,4,6
            dowmatch (match-field :dow (.get cal (Calendar/DAY_OF_WEEK)) sched)]
        (and hourmatch minmatch dommatch monmatch dowmatch))))
 
-(defn ^{:nodoc true} get-runable [tasks]
+(defn ^{:no-doc true} get-runable [tasks]
   (filter (fn [r] (runs-now? (:schedule r))) tasks))
 
-(defn ^{:nodoc true} do-run-func [task]
+(defn ^{:no-doc true} do-run-func [task]
   ((:runfunction task)))
 
-(defn ^{:nodoc true} get-sleep-until-next-minute []
+(defn ^{:no-doc true} get-sleep-until-next-minute []
   (let [ci (Calendar/getInstance)]
     (* 1000 (- 60 (.get ci (Calendar/SECOND))))))
 
 (defn running?
   "returns true if the runner is running"
-  []
+  []1
   (not (= nil @*runner*)))
 
 (defn start-runner
